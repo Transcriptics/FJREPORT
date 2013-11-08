b886fa844f96af70b6b1c90b1f5c5c77ac56219b
Esteban Garro <egarro@transcriptics.com>
2013-11-07 23:43:26 -0500

    Added a time metric for our testing case

--- src/net/sf/fjreport/cell/Cell.java
+++ src/net/sf/fjreport/cell/Cell.java
@@ -71,7 +71,7 @@ public class Cell extends Rectangle{
 	public static final int ALIGN_RIGHT = 2;
 	private Font font = defaultFont;
 
-	private boolean selecteda;
+	private boolean selected;
 	
 	private String[] comboxOptions;
 	private static final Insets DEFAULT_MARGIN = new Insets(0, 0, 0, 0);
